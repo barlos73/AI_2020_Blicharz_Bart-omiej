c6fea8d58788bcde9ff22ee699aab5a5889ac6bb
barlos73 <bblicharz@interia.pl>
2020-04-14 16:21:20 +0200

    Akutalizacja

--- php/rejestracja.php
+++ php/rejestracja.php
@@ -9,57 +9,57 @@ if(isset($_POST['rejestruj'])){
 	$passwordRepeat = $_POST['haslo2'];
 	
 if(empty($username)|| empty($email) || empty($password) || empty($passwordRepeat)){
-	header("Location: ../index.php?error=emptyfield&login=".$username."&email=".$email);
+	header("Location: ../index.html?error=emptyfield&login=".$username."&email=".$email);
 	exit();
 }
 else if(!filter_var($email, FILTER_VALIDATE_EMAIL)&& !preg_match("/^[a-zA-Z0-9]*$/",$username)){
-	header("Location: ../index.php?error=invalidemaillogin");
+	header("Location: ../index.html?error=invalidemaillogin");
 	exit();
+	
 }
 else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-	header("Location: ../index.php?error=invalidemail&login=".$username);
+	header("Location: ../index.html?error=invalidemail&login=".$username);
 	exit();
+	
 }
 else if (!preg_match("/^[a-zA-Z0-9]*$/",$username)){
-	header("Location: ../index.php?error=invalidlogin&email=".$email);
+	header("Location: ../index.html?error=invalidlogin&email=".$email);
 	exit();
+	
 }
 else if($password !== $passwordRepeat){
-	header("Location: ../index.php?error=passwordchecklogin=".$username."&email=".$email);
+	header("Location: ../index.html?error=passwordcheck&login=".$username."&email=".$email);
 	exit();
+	
 }
 else {
-
-	$sql ="SELECT id FROM uzytkownik WHERE id=?";
-	$stmt =mysqli_stmt_init($conn);
-	
+	$sql ="SELECT login FROM uzytkownik WHERE login=?";
+	$stmt =mysqli_stmt_init($conn);	
 if (!mysqli_stmt_prepare($stmt,$sql)){
-	header("Location: ../index.php?error=sqlerror");
+	header("Location: ../index.html?error=sqlerror");
 	exit();
 }
 else {
 	mysqli_stmt_bind_param($stmt,"s",$username);
 	mysqli_stmt_execute($stmt);
 	mysqli_stmt_store_result($stmt);
-	$resultCheck = mysqli_stmt_num_rows();
-if($resultCheck > 0){
-	header("Location: ../index.php?error=usertaken&email=".$email);
+	$resultCheck = mysqli_stmt_num_rows($stmt);
+if($resultCheck>0){
+	header("Location: ../index.html?error=usertaken&email=".$email);
 	exit();
 	}
-else {
-		
-		$sql = "INSTER INTO uzytkownik (login,email,haslo) VALUES(?, ?, ?)";
-		$stmt =mysqli_stmt_init();
+else {	
+		$sql ="INSERT INTO uzytkownik (login,email,haslo) VALUES(?,?,?)";
+		$stmt =mysqli_stmt_init($conn);
 if (!mysqli_stmt_prepare($stmt,$sql)){
-	header("Location: ../index.php?error=sqlerror");
+	header("Location: ../index.html?error=sqlerror");
 	exit();
 	}
 else {
 	$hashedPwd = password_hash($password, PASSWORD_DEFAULT);
-	
 	mysqli_stmt_bind_param($stmt,"sss",$username,$email,$hashedPwd);
 	mysqli_stmt_execute($stmt);
-	header("Location: ../index.php?signup=success");
+	header("Location: ../index.html?signup=success");
 	exit();
 	}
 }
@@ -69,7 +69,6 @@ mysqli_stmt_close($stmt);
 mysqli_close($conn);
 }
 else {
-	header("Location: ../index.php");
+	header("Location: ../index.html");
 	exit();
 }
-?>
--- php/zaloguj.php
+++ php/zaloguj.php
@@ -1,5 +1,50 @@
-﻿<? php
-$login=$_POST['login'];
-$haslo=$_POST['haslo'];
-
-?>
+﻿<?php
+if(isset($_POST['zaloguj'])){
+	require 'polaczenie.php';
+	$mailuid = $_POST['loginemail'];
+	$password = $_POST['haslo'];
+if(empty($mailuid)|| empty($password)){
+	header("Location: ../logowanie.html?error=emptyfields");
+	exit();
+	}
+	else {
+		$sql = "SELECT * FROM uzytkownik WHERE login=? OR email=? ";
+		$stmt = mysqli_stmt_init($conn);
+		if(!mysqli_stmt_prepare($stmt,$sql)){
+			header("Location: ../logowanie.html?error=sqlerror");
+			exit();
+		}
+		else {
+			
+			mysqli_stmt_bind_param($stmt,"ss",$mailuid,$mailuid);
+			mysqli_stmt_execute($stmt);
+			$result = mysqli_stmt_get_result($stmt);
+			if($row = mysqli_fetch_assoc($result)){
+				$pwdCheck = password_verify($password,$row['haslo']);
+				if($pwdCheck == false){
+					header("Location: ../logowanie.html?error=wrongpwd");
+					exit();
+				}
+				else if($pwdCheck == true){
+					session_start();
+					$_SESSION['id'] = $row['id'];
+					$_SESSION['login'] = $row['login'];
+					header("Location: ../glowna.html?login=success");
+					exit();
+				}
+				else {
+					header("Location: ../logowanie.html?error=wrongpwd");
+					exit();
+				}
+			}
+			else{
+				header("Location: ../logowanie.html?error=nouser");
+				exit();
+			}
+		}
+	}
+}
+else{
+	header("Location: ../glowna.html");
+	exit();
+}
